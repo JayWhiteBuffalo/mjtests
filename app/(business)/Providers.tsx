1f3a03abdb5934b6573f61a9ab585b401e7264ed
David He <dhe128@gmail.com>
2024-04-28 12:28:35 -0700

    remove auth provider

--- app/(business)/Providers.tsx
+++ app/(business)/Providers.tsx
@@ -1,12 +1,9 @@
-import {AuthProvider} from '@components/auth/AuthProvider'
 import {NextUIProvider} from '@nextui-org/react'
 import {ThemeProvider as NextThemesProvider} from 'next-themes'
 
 export const Providers = ({children}) =>
   <NextUIProvider>
     <NextThemesProvider attribute="class" defaultTheme="light">
-      <AuthProvider>
-        {children}
-      </AuthProvider>
+      {children}
     </NextThemesProvider>
   </NextUIProvider>
--- app/admin/Providers.tsx
+++ app/admin/Providers.tsx
@@ -1,4 +1,3 @@
-import {AuthProvider} from '@components/auth/AuthProvider'
 import {MantineProvider} from '@mantine/core'
 import {NextUIProvider} from '@nextui-org/react'
 import {ThemeProvider as NextThemesProvider} from 'next-themes'
@@ -7,9 +6,7 @@ export const Providers = ({children}) =>
   <NextUIProvider>
     <NextThemesProvider attribute="class" defaultTheme="light">
       <MantineProvider>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        {children}
       </MantineProvider>
     </NextThemesProvider>
   </NextUIProvider>
