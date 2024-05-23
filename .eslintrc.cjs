444969bccc8bdc0592190f48753f3c89f838b4da
David He <dhe128@gmail.com>
2024-05-22 18:00:07 -0700

    eslint, tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,47 @@
 module.exports = {
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'import', 'jsx-a11y'],
-  'extends': [
+  extends: [
     'eslint:recommended',
-    // 'plugin:react/recommended',
-    // 'plugin:react-hooks/recommended',
+    // "plugin:react/recommended",
+    // "plugin:react-hooks/recommended",
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
     'plugin:jsx-a11y/recommended',
     'prettier',
     'next/core-web-vitals',
   ],
-  'rules': {
-    'import/no-anonymous-default-export': 'off',
-    'no-mixed-operators': 'off',
-    'no-restricted-globals': 'off',
-    'react/display-name': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'no-undef': 'warn',
-    'jsx-a11y/no-autofocus': 'warn',
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'import', 'jsx-a11y'],
+  rules: {
+    '@next/next/no-html-link-for-pages': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
-        'varsIgnorePattern': '^_',
-        'argsIgnorePattern': '^_',
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       },
     ],
+    'import/no-anonymous-default-export': 'off',
+    'jsx-a11y/label-has-associated-control': 'warn',
+    'jsx-a11y/no-autofocus': 'warn',
+    'no-empty': 'off',
+    'no-extra-boolean-cast': 'warn',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useMemoWithDispose',
+      },
+    ],
+  },
+
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
